752babfe4c806afc535682e7d5bc301c22b50447
Rohith <gambol99@gmail.com>
2016-03-07 17:53:19 +0000

    - adding the sed replacing to the rbd command

--- client/bin/run.sh
+++ client/bin/run.sh
@@ -5,6 +5,10 @@
 #
 #  vim:ts=2:sw=2:et
 #
+# step: somewhat hacky where to pass down the version and registry
+sed -i "s/%REGISTRY%/${REGISTRY}/" /bin/rbd
+sed -i "s/%TAG%/${TAG}/" /bin/rbd
+
 cp /bin/rbd /opt/bin/${i} && chmod +x /opt/bin/${i}
 
 # loop forever until the container is stopped
